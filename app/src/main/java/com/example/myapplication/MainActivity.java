ec243eb2169ac3da7184d38e9d8a72cd6a59b100
Lior Vaknin <liorvak303@gmail.com>
2020-01-05 12:02:23 +0200

    added and fixed server calc

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,26 +12,122 @@ import android.widget.Button;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.RetryPolicy;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.gson.JsonIOException;
+
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
     private FusedLocationProviderClient fusedLocationClient;
     private Button findHospital;
     private Location loc;
-    private static final String URL = "http://192.168.137.1:5000/";
+    private static final String URL = "http://10.200.202.149:5000/";
+    private static String coords[] = new String[2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        findHospital = findViewById(R.id.findHospital);
+        findHospital.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+                //
+                // find local position
+                //
+
+                //
+                // post the server
+                //
+
+                //
+                // call to server
+                http_get(URL);
+                //
+
+                //
+                // start waze
+                //
+            }
+        });
+    }
+
+    private void http_get(String URL) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("Debug", response.toString());
+                        try{
+                            coords[0] = response.getString("lat");
+                            coords[1] = response.getString("lon");
+                            String uri = "waze://?ll=" + coords[0] + ", " + coords[1] + "&navigate=yes";
+                            startActivity(new Intent(android.content.Intent.ACTION_VIEW,
+                                    Uri.parse(uri)));
+                        }
+                        catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Debug", error.toString());
+                    }
+                }
+        );
+        objectRequest.setRetryPolicy(new RetryPolicy() {
+            @Override
+            public int getCurrentTimeout() {
+                return 50000;
+            }
+
+            @Override
+            public int getCurrentRetryCount() {
+                return 50000;
+            }
+
+            @Override
+            public void retry(VolleyError error) throws VolleyError {
+
+            }
+        });
+        requestQueue.add(objectRequest);
+    }
+
+    private void http_post(String URL, String data){
+
+    }
+
+    private void temp(){
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
         findHospital = findViewById(R.id.findHospital);
@@ -50,31 +149,5 @@ public class MainActivity extends AppCompatActivity {
                         });
             }
         });
-    }
-
-    private void http_get(String URL) {
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        JsonObjectRequest objectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                URL,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d("Debug", response.toString());
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.d("Debug", error.toString());
-                    }
-                }
-        );
-        requestQueue.add(objectRequest);
-    }
-
-    private void http_post(String URL, String data){
-        CallAPI callAPI = new CallAPI();
-        callAPI.doInBackground(URL, data);
     }
 }
--- python server/example.py
+++ python server/example.py
@@ -7,22 +7,44 @@ from flask import Flask
 from flask import request
 app = Flask(__name__)
 
-from_address = 'Ashqelon, Israel'
-to_address = 'Ashdod, Israel'
+list_of_hospital = ['בית חולים קפלן, ישראל',
+                    'בית חולים סורוקה, ישראל',
+                    'בית חולים אסותא אשדוד, ישראל',
+                    'בית חולים הדסה, ישראל']
+
+from_address = 'Ashdod, Israel'
+to_address = None
+
+
+def find_minimum_destination():
+    minimum = 800
+    index = 0
+    coords = 0
+    for i in range(len(list_of_hospital)):
+        to_address = list_of_hospital[i]
+        route = WazeRouteCalculator.WazeRouteCalculator(from_address, to_address)
+        if minimum > route.calc_route_info()[0]:
+            minimum = route.calc_route_info()[0]
+            index = i
+            coords = route.address_to_coords(list_of_hospital[i])
+    return index, coords
 
 
 @app.route('/', methods=['GET', 'POST'])
 def user_function():
     if request.method == 'GET':
-        """return the information for <user_id>"""
-        return "{""hello"":1}"
+        hospital = find_minimum_destination()
+        print("$$$$$$$$$$$$$$$$$$$$$")
+        print(hospital[0])
+        print(hospital[1])
+        return "{}".format(hospital[1])
     if request.method == 'POST':
         """modify/update the information for <user_id>"""
         # you can use <user_id>, which is a str but could
         # changed to be int or whatever you want, along
         # with your lxml knowledge to make the required
         # changes
-        from_address = request.form # a multidict containing POST data
+        from_address = request.form  # a multidict containing POST data
     else:
         # POST Error 405 Method Not Allowed
         return None
@@ -33,10 +55,8 @@ logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 logger.addHandler(handler)
 
-route = WazeRouteCalculator.WazeRouteCalculator(from_address, to_address)
 
 try:
-    route.calc_route_info()
-    app.run(host='192.168.137.44')
+    app.run(host='10.200.202.149')
 except WazeRouteCalculator.WRCError as err:
     print(err)
