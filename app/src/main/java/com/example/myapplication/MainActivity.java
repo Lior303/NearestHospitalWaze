38100c2898914c5defe47e90567ef8d4dab192e8
Lior Vaknin <liorvak303@gmail.com>
2020-01-06 11:59:16 +0200

    ready to go

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
@@ -10,6 +12,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -43,14 +47,54 @@ import javax.net.ssl.HttpsURLConnection;
 public class MainActivity extends AppCompatActivity {
 
     private Button findHospital;
+    private Button getHelp;
+    private Button about;
     private static final String URL = "http://192.168.137.44:5000/";
     private static String coords[] = new String[2]; // first lat than lon
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        about = findViewById(R.id.about);
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Nearest Hospital with Waze");
+                alertDialog.setMessage("This application find the nearest hospital (in terms of ETA) from the user's" +
+                        " current location. Than, the application starts Waze navigation towards this hospital.");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
+
+        getHelp = findViewById(R.id.getHelp);
+        getHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+                alertDialog.setTitle("Nearest Hospital with Waze");
+                alertDialog.setMessage("This application was created by Lior Vaknin and Mordy Dabah, " +
+                        "as part of the course Geolocation and IoT.");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         findHospital = findViewById(R.id.findHospital);
         findHospital.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -58,8 +102,20 @@ public class MainActivity extends AppCompatActivity {
 
                 //
                 // find local position
-                coords[0] =  "31.79300880432129";
-                coords[1] = "34.64921951293945";
+                Toast.makeText(MainActivity.this, "Start calculating...", Toast.LENGTH_LONG).show();
+                findHospital.setEnabled(false);
+
+                fusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                // Got last known location. In some rare situations this can be null.
+                                if (location != null) {
+                                    coords[0] = Double.toString(location.getLatitude());
+                                    coords[1] = Double.toString(location.getLongitude());
+                                }
+                            }
+                        });
 
                 //
                 // post to server
@@ -119,10 +175,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         requestQueue.add(objectRequest);
-    }
-
-    private void get_location(){
-
     }
 
     private void startWaze(){
